9dffc64a28ebe3335c06448572995f3025cbf098
Ajinkya Kolhe <ajinkyakolhe112@gmail.com>
2020-04-26 20:07:41 +0530

    Update head.js

--- src/head.js
+++ src/head.js
@@ -1,4 +1,4 @@
-const endpoint = 'https://www.jsonstore.io/6b6e3cd07f3aaecd8df25ab95875eb2462f00a7614f7205c2d5dc5229896ea98'
+const endpoint = 'https://www.jsonstore.io/984265cf278a2655f7f3d3c740ff9227152aff9d4d2d47f0359ff2c89357f9d0'
 const ebox = document.getElementById('erbox')
 const fetchJSON = (a) => {
 	const f = new XMLHttpRequest()
